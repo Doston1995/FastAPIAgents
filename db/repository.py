19ed49e5e5eecbd67e537f31456f7b361a8e4def
Doston1995 <ctron1108@gmail.com>
2022-10-09 15:17:04 +0500

    order update and delete

--- db/repository.py
+++ db/repository.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
-from db.models import AGENT, CUSTOMER
-from db.schemas import AgentCreate, AgentShow, CustomerCreate, CustomerShow
+from db.models import AGENT, CUSTOMER, ORDER
+from db.schemas import AgentCreate, AgentShow, CustomerCreate, CustomerShow, OrderCreate, OrderShow
 
 
 ###################### Agents ###################
@@ -79,3 +79,39 @@ def delete_customer(cust_code:str, db: Session):
 
 
 ###################### Orders  ###################
+
+
+def list_orders(db : Session):
+    orders = db.query(ORDER).all()
+    return orders
+
+
+def create_order(order: OrderCreate, db: Session):
+    order_object = ORDER(**order.dict())
+    db.add(order_object)
+    db.commit()
+    db.refresh(order_object)
+    return order_object
+    
+
+def retreive_order(ord_num:str, db:Session):
+    order = db.query(ORDER).filter(ORDER.ORD_NUM == ord_num).first()
+    return order
+
+
+def update_order(ord_num:str, order: OrderShow, db: Session):
+    existing_order = db.query(ORDER).filter(ORDER.ORD_NUM == ord_num)
+    if not existing_order.first():
+        return 0
+    existing_order.update(order.__dict__)
+    db.commit()
+    return 1
+
+
+def delete_order(ord_num:str, db: Session):
+    existing_order = db.query(ORDER).filter(ORDER.ORD_NUM == ord_num)
+    if not existing_order.first():
+        return 0
+    existing_order.delete(synchronize_session=False)
+    db.commit()
+    return 1
--- routers/route_orders.py
+++ routers/route_orders.py
@@ -3,6 +3,49 @@ from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, status
 from db.session import get_db
 from typing import List 
+from db.schemas import OrderShow, OrderCreate
+from db.repository import list_orders, create_order, retreive_order, update_order, delete_order
 
 
 router = APIRouter()
+
+
+@router.get("/all", response_model=List[OrderShow])
+def get_all_orders(db:Session = Depends(get_db)):
+    orders = list_orders(db=db)
+    return orders
+
+
+@router.post("/create/",response_model=OrderShow)
+def create_orders(order: OrderCreate, db: Session = Depends(get_db)):
+    order = create_order(order=order, db=db)
+    return order
+
+
+@router.get("/customer/{ord_num}",response_model=OrderShow)
+def read_customer(ord_num:str, db:Session = Depends(get_db)):
+    order = retreive_order(ord_num=ord_num, db=db)
+    if not order:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Order with this number {ord_num} does not exist")
+    return order
+
+
+@router.put("/update/{ord_num}") 
+def update_orders(ord_num:str, order: OrderCreate, db: Session = Depends(get_db)):
+    message = update_order(ord_num=ord_num,order=order,db=db)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Order with number {ord_num} not found")
+    return {"msg":"Successfully updated data."}
+
+
+@router.delete("/delete/{ord_num}")
+def delete_orders(ord_num:str, db: Session = Depends(get_db)):
+    order = retreive_order(ord_num=ord_num,db=db)
+    if not order:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Order with this number {ord_num} does not exist")
+    if order:
+        delete_order(ord_num=ord_num,db=db)
+        return {"detail": "Successfully deleted."} 
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail=f"You are not permitted!!!!")
